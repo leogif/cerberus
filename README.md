a7db6869c1bf9220e0b017c84d58dab17ff7cc02
leogif <leogif@gmail.com>
2017-08-15 21:42:48 -0300

    1 commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# octopus 
+
+@ octupus
+#
+#
+
+# Cerberus
---
+++ js/main.js
@@ -0,0 +1,13 @@
+function goTo(element, speed){
+  var href = element.attr('href');
+  var top = $(href).offset().top;
+
+    $("html,body").animate({scrollTop : top}, speed)
+}
+
+$(function(){
+  $("#top a").click(function(e){
+    e.preventDefault();
+    goTo($(this), 500);
+  });
+});
